d7b2e9e4ff00a8c01cc3c872b23b1663016f6097
Henry Schreiner <henryfs@princeton.edu>
2026-03-24 10:33:51 -0400

    refactor: move to file

--- src/flake8_lazy/analysis.py
+++ src/flake8_lazy/analysis.py
@@ -2,6 +2,13 @@
 
 from __future__ import annotations
 
+__lazy_modules__ = [
+    "ast",
+    f"{__spec__.parent}.ast_helpers",
+    f"{__spec__.parent}.bindings",
+    f"{__spec__.parent}.visitors",
+]
+
 import ast
 from typing import TYPE_CHECKING
 
